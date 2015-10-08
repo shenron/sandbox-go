4b7d65b0d5a5536d54c3745f34d89dbe5444a839
Shenron <thomas@castelly.fr>
2015-10-08 23:01:20 +0200

    add goroutine simple sample

---
+++ goroutines/simple/output.md
@@ -0,0 +1,5 @@
+```shell
+- Hey I need help, calculate this please !
+- No problem, but I restart my WAS wait some hours ...
+ ... Result  7
+```
---
+++ goroutines/simple/simple.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"math/big"
+	"crypto/rand"
+)
+
+func Calculate() int64 {
+	c := big.NewInt(10)
+	fakeResult, _ := rand.Int(rand.Reader, c)
+	return fakeResult.Int64()
+}
+
+func HelpCalculate(c chan int64, fct func() int64) {	
+	fmt.Println("- No problem, but I restart my WAS wait some hours ...")
+	seconds := 5 
+	time.Sleep(time.Duration(seconds) * time.Second)
+	c <- fct()
+}
+
+func main() {
+	fmt.Println("- Hey I need help, calculate this please !")
+
+	c := make(chan int64)
+	go HelpCalculate(c, Calculate)
+	result := <- c
+
+	fmt.Println(" ... Result ", result)
+}
