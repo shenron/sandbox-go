54cf6a966c415a21459c4d0b47874789e0b8d10d
Thomas CASTELLY <shenron@tcy.io>
2015-10-12 21:54:19 +0200

    mv output to README

--- goroutines/select/select.go
+++ goroutines/select/select.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"time"
-	"math/big"
 	"crypto/rand"
+	"fmt"
+	"math/big"
+	"time"
 )
 
-func Grenade(c chan int64){
+func Grenade(c chan int64) {
 	delay, _ := rand.Int(rand.Reader, big.NewInt(10))
 	seconds := delay.Int64()
 	fmt.Printf("Grenade : Boom after %d seconds\n", seconds)
@@ -25,17 +25,16 @@ func main() {
 	i := 0
 	for {
 		select {
-		case r1 := <- c1 :
+		case r1 := <-c1:
 			fmt.Printf("Boomed in %d seconds\n", r1)
-		case r2 := <- c2 :
+		case r2 := <-c2:
 			fmt.Printf("Boom in %d seconds\n", r2)
-		default :
+		default:
 			i += 1
 			time.Sleep(1 * time.Second)
 			if i >= 10 {
 				fmt.Println("TIME OUT\n", i)
-				return;
-			
+				return
 			} else {
 				fmt.Printf("%d ...\n", i)
 			}
