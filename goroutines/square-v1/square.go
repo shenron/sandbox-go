39ccfe650e45a4c8bdf234cb280d1bca440066eb
Thomas CASTELLY <shenron@tcy.io>
2015-10-12 21:53:07 +0200

    sample routine for calculate square

---
+++ goroutines/square-v1/square.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+/**
+* Generate chanel
+ */
+func generate(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func square(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func main() {
+	c := generate(2, 4)
+	out := square(c)
+
+	for i := 0; i < 2; i++ {
+		fmt.Println(<-out)
+	}
+	// 4 16
+}
